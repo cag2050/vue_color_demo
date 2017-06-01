b52391deea6fa6a5601ff77cb6700aca18e52fea
cag2050 <cag2050@163.com>
2017-06-01 14:44:57 +0800

    m

---
+++ src/components/Vuecolor.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+    <chrome-picker :value="colors" @input="updateValue"></chrome-picker>
+</div>
+</template>
+
+<script>
+import { Chrome } from 'vue-color'
+
+let defaultProps = {
+    hex: '#194d33',
+    hsl: {
+        h: 150,
+        s: 0.5,
+        l: 0.2,
+        a: 1
+    },
+    hsv: {
+        h: 150,
+        s: 0.66,
+        v: 0.30,
+        a: 1
+    },
+    rgba: {
+        r: 25,
+        g: 77,
+        b: 51,
+        a: 1
+    },
+    a: 1
+}
+export default {
+    components: {
+        'chrome-picker': Chrome
+    },
+    data () {
+        return {
+            colors: defaultProps
+        }
+    },
+    methods: {
+        updateValue (value) {
+            console.log(value)
+            this.colors = value
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
